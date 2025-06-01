e493d5ddf528f0c8fea71984a61649361b9393f1
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-06-01 18:25:27 +0530

    init project, bootstrap graphql

---
+++ graphql/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/99designs/gqlgen/handler"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type AppConfig struct {
+	AccountURL string `envconfig:"ACCOUNT_SERVICE_URL"`
+	CatalogURL string `envconfig:"CATALOG_SERVICE_URL"`
+	OrderURL   string `envconfig:"ORDER_SERVICE_URL"`
+}
+
+func main() {
+	var cfg AppConfig
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server, err := NewGraphQLServer(cfg.AccountURL, cfg.CatalogURL, cfg.OrderURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/graphql", handler.GraphQL(server.ToExecutableSchema()))
+	http.Handle("/playground", playground.Handler("GraphQL Playground", "/graphql"))
+
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ graphql/query_resolver.go
@@ -0,0 +1,14 @@
+package main
+
+import "context"
+
+type queryResolver struct {
+	server *Server
+}
+
+func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string) ([]*Account, error) {
+}
+
+func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput, query *string) ([]*Product, error) {
+
+}
